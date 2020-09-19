8924d5d75ea972b69b88c7fbad9ed3ee0cf75375
Denis Medeiros <contato.denismedeiros@hotmail.com>
2020-09-19 15:32:37 -0300

    port heroku

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,9 @@ import connection from './database/connection'
 const server = express()
 
 server.use(express.json())
-server.use(cors())
+server.use(cors({origin: 'https://helpper-fr.herokuapp.com/'}))
 connection()
 
 server.use(routes)
 
-server.listen(3333, () => {
-  console.log('Server start, port 3333')
-})
+server.listen(process.env.PORT ? process.env.PORT : 3333)
