fb66ad3b4519e6567fc118900eef7c922023a6e8
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2020-09-20 09:08:51 -0300

    add dotenv

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv'
 import mongoose from "mongoose";
 
+dotenv.config()
+
 function connection() {
-  mongoose.connect("mongodb+srv://helpper:drGuYNAIK6vCNoqf@cluster0.hr2tt.mongodb.net/helpper?retryWrites=true&w=majority", {
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.hr2tt.mongodb.net/helpper?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,14 @@
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
-import mongoose from 'mongoose';
-// import connection from './database/connection'
-
-const uri = process.env.MONGO_URL
-  ? process.env.MONGO_URL
-  : 'mongodb+srv://helpper:drGuYNAIK6vCNoqf@cluster0.hr2tt.mongodb.net/helpper?retryWrites=true&w=majority';
+import connection from './database/connection'
 
 const server = express()
 
 server.use(express.json())
 server.use(cors())
 
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+connection()
 
 server.use(routes)
 
